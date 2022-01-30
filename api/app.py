158f39b34da35ef77ac0f38d5f8552a6f8480a4e
lux-sales <sales_l@hotmail.com>
2022-01-30 18:23:19 -0300

    adding gitignore and requirements.txt

--- api/app.py
+++ api/app.py
@@ -1,16 +1,12 @@
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
-from bson.json_util import dumps
 from bson.objectid import ObjectId
-from flask_appbuilder import AppBuilder
-from flask_mongoengine import MongoEngine
-from flask_appbuilder.security.mongoengine.manager import SecurityManager
 
 app = Flask(__name__)
 
 app.secret_key = "secretkey"
 
-app.config["MONGO_URI"] = "mongodb+srv://lucas:1234@desafio-freshmania.gcleq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://lucas:1234@desafio-freshmania.gcleq.mongodb.net/freshmania?retryWrites=true&w=majority"
 
 mongo = PyMongo(app)
 

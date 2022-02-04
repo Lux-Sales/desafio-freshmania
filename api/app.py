d715d3b863da501b20ea40b20fe0256f3f071e69
lux-sales <sales_l@hotmail.com>
2022-02-04 17:17:14 -0300

    modificando CORS, atualizando dockerfile e atualizando requirements

--- api/app.py
+++ api/app.py
@@ -2,8 +2,10 @@ import os
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.secret_key = os.getenv('SECRET_KEY')
 
